dc51b090c4dc0119a7afd011d6cfd4acd9b8e7a5
Bharat Dua <bharatdua13@gmail.com>
2024-06-03 10:19:26 +0530

    setting up routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,18 @@ import ForgetPassword from "./Components/ForgetPassword/ForgetPassword";
 import Home from "./Components/Home/Home";
 import Login from "./Components/Login/Login";
 import Signup from "./Components/Signup/Signup";
-
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 const App = () => {
   return (
-    <div>
-      <Login />
-      <Signup />
-      <ForgetPassword />
-      <Home />
-    </div>
+   <Router>
+    <Routes>
+      <Route path="/" element={<Login/>}/>
+      <Route path="/home" element={<Home/>}/>
+      <Route path="/signup" element={<Signup/>}/>
+      <Route path="/forget-password" element={<ForgetPassword/>}/>
+
+    </Routes>
+   </Router>
   );
 };
 

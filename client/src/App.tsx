bff6e903741d361e852dad579b8728cf534646da
Bharat Dua <bharatdua13@gmail.com>
2024-06-03 09:46:50 +0530

    creating components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,17 @@
+import ForgetPassword from "./Components/ForgetPassword/ForgetPassword";
+import Home from "./Components/Home/Home";
+import Login from "./Components/Login/Login";
+import Signup from "./Components/Signup/Signup";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Login />
+      <Signup />
+      <ForgetPassword />
+      <Home />
+    </div>
+  );
 };
 
 export default App;

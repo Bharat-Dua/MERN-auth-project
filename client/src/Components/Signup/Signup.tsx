935533a72578236340db8435b17ed3f69ed9bfa7
Bharat Dua <bharatdua13@gmail.com>
2024-06-03 19:12:29 +0530

    taking signup form input data

--- client/src/Components/Signup/Signup.tsx
+++ client/src/Components/Signup/Signup.tsx
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useState, ChangeEvent } from "react";
 
 import styles from "./Signup.module.css";
 import { Link } from "react-router-dom";
 const Signup = () => {
+  const [userDetails, setUserDetail] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>): void {
+    const { name, value } = event.target;
+    // console.log(name, value);
+    setUserDetail((prev) => ({ ...prev, [name]: value }));
+    console.log(userDetails)
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.formContainer}>
         <h2>Sign up...</h2>
         <div className={styles.inputContainer}>
-          <input type="text" placeholder="Enter your name..." />
-          <input type="email" placeholder="Enter your email..." />
-          <input type="password" placeholder="Enter your password..." />
+          <input
+            type="text"
+            value={userDetails.username}
+            onChange={handleInputChange}
+            name="username"
+            placeholder="Enter your name..."
+          />
+          <input
+            type="email"
+            value={userDetails.email}
+            onChange={handleInputChange}
+            name="email"
+            placeholder="Enter your email..."
+          />
+          <input
+            type="password"
+            value={userDetails.password}
+            onChange={handleInputChange}
+            name="password"
+            placeholder="Enter your password..."
+          />
           <button>Sign up</button>
         </div>
         <span>

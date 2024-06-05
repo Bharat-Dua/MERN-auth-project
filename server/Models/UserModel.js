7a9afa2ed9e32887ee277be45efcd0c7b9e190be
Bharat Dua <bharatdua13@gmail.com>
2024-06-05 16:37:29 +0530

    creating user model

---
+++ server/Models/UserModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  OTP_VerificationToken: {
+    OTP: String,
+    expires: Date,
+  },
+  VerificationToken: {
+    token: String,
+    expires: Date,
+  },
+});
+
+const UserModel = mongoose.model("Users", UserSchema);
+
+module.exports = UserModel

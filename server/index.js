03e5059ef1d6ed074ff25c95c1fb0b646a9c37af
Bharat Dua <bharatdua13@gmail.com>
2024-06-05 11:07:32 +0530

    test route

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ const app = express();
 
 app.use(morgan("dev"));
 app.use(cors());
-
+app.use(express.json());
+app.get("/test", (req, res) => {
+  res.status(200).json({ test: "success" });
+});
 const port = process.env.PORT || 5000;
 app.listen(port, console.log(`server is running at ${port} port`));
